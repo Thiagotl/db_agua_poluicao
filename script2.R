d78db679c4cbbe3eaa3427e8614d707f40415aab
Thiago Tavares <thiago.tlopess@gmail.com>
2025-01-09 15:44:07 -0300

    first commit

---
+++ script2.R
@@ -0,0 +1,12 @@
+library(readr)
+
+dados_sisagua_p7 <- read_csv("planilha7_pivotresult_sisagua_25_out (1).csv")
+View(dados_sisagua_p7)
+
+colnames(dados_sisagua_p7)[colnames(dados_sisagua_p7)=='município']<-'municipio'
+colnames(quant_cnaes)[colnames(quant_cnaes)=='municipio_nome']<-'municipio'
+
+
+dados_combinados <- dados_sisagua_p7 |> 
+  left_join(quant_cnaes, by='municipio')
+
