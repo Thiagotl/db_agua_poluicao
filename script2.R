9ea740022c64d9934e367a0e27ce000cfb36876b
Thiago Tavares <thiago.tlopess@gmail.com>
2025-01-16 16:08:54 -0300

    att

--- script2.R
+++ script2.R
@@ -17,3 +17,24 @@ View(dados_combinados)
 
 contagem_substancias<-dados_combinados |> 
   count(parâmetro)
+
+
+
+dados_combinados_caracter <- dados_combinados %>%
+  mutate(across(where(is.numeric), as.character))
+
+
+contagem_cnaes_mun<-dados_combinados_caracter |> 
+  pivot_longer(
+    cols = -c(nome_da_instituição,sigla_da_instituição, cnpj_da_instituição, `nome_do_escritório_regional/local`,
+              `cnpj_do_escritório_regional/local`, nome_da_forma_de_abastecimento, `nome_da_eta_/_uta`, código_ibge,
+              grupo_de_parâmetros, `Total de testes substâncias em geral para cada linha - incluindo MENOR_LQ`, `Total de inconsistentes`,
+              `Porcentagem de inconsistentes`, `Total de Consistentes não detectados`, `Porcentagem de Consistentes não detectados`,
+              `Total de parâmetros com MENOR_LQ`, `Porcentagem de parâmetros com MENOR_LQ`, `Total de Consistentes não detectados + MENOR_LQ`,
+              `Porcentagem Consistentes não detectados + MENOR_LQ`, `Total de Consistentes detectados Abaixo do VMP`, `Porcentagem de Consistentes detectados Abaixo do VMP`,
+              `Total de Consistentes detectados Acima do VMP`, `Porcentagem de Consistentes detectados Acima do VMP`),
+    names_to = "CNAE",
+    values_to = "quantidade",
+    names_transform = list(CNAE = as.character)
+  ) |> 
+  filter(quantidade > 0)
