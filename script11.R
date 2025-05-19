f2f48381c026838d136785d7690a1eafa6779645
Thiago Tavares <thiago.tlopess@gmail.com>
2025-05-19 15:44:03 -0300

    att

---
+++ script11.R
@@ -0,0 +1,37 @@
+library(readxl)
+library(tidyverse)
+
+
+dados <- read_excel("municipios_area_planta_area_colhida.xlsx")
+View(dados)
+
+
+# CULTURAS TEMPORARIAS-------------
+
+dados_area_temporaria<-dados |> 
+  filter(tipo_lavoura == "Temporária") 
+dados_area_temporaria<-dados_area_temporaria[,1:10]
+
+dados_area_temporaria_winde<- dados_area_temporaria |> 
+  pivot_wider(
+    names_from = `Produto das lavouras temporárias e permanentes`,
+    values_from = Valor,
+    values_fill = 0
+  )
+
+
+# CULTARAS PERMANENTES -------------------
+
+dados_area_permanente <- dados |> 
+  filter(tipo_lavoura == "Permanente")
+
+dados_area_permanente<-dados_area_permanente |> 
+  select(-c(`Produto das lavouras temporárias e permanentes`))
+
+
+dados_area_permanente_winde<- dados_area_permanente |> 
+  pivot_wider(
+    names_from = `Produto das lavouras permanentes`,
+    values_from = Valor,
+    values_fill = 0
+  )
